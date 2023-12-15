71da62a16003451f25b94efbc8a795813869584c
Vivek-Kalal-Mobiuso <152871736+Vivek-Kalal-Mobiuso@users.noreply.github.com>
2023-12-15 17:08:25 +0530

    Delete routes directory

--- routes/customerRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import newCustomer from '../controllers/customerControllers.js'
-import * as validate from "../middlewares/validate.js";
-
-const route = express.Router()
-
-route.post("/", validate.validateCustomer , newCustomer );
-
-export default route;
--- routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-// import placeOrder from '../controllers/orderControllers.js'
-import * as orderController from '../controllers/orderControllers.js'
-const route = express.Router()
-
-route.post("/", orderController.placeOrder );
-
-export default route;
